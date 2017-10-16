3f79cc81af86080b4b9e73dfdf5723f1d88e1d8f
mateuszkopacz <m.kopacz199@gmail.com>
2017-10-16 22:47:44 +0200

    Inital Commit

--- CropImage/ViewController.swift
+++ CropImage/ViewController.swift
@@ -8,11 +8,74 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
 
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var imageViewWidth: NSLayoutConstraint!
+    
+    @IBOutlet weak var imageViewHeight: NSLayoutConstraint!
+    
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    func setImageToCrop(image: UIImage){
+
+        imageView.image = image
+
+        imageViewWidth.constant = image.size.width
+        imageViewHeight.constant = image.size.height
+
+        let scaleHeight = scrollView.frame.width / image.size.width
+        let scaleWidth = scrollView.frame.height / image.size.height
+
+        let maxScale = max(scaleWidth, scaleHeight)
+
+        scrollView.zoomScale = maxScale
+
+       
+
+
+    }
+ 
+    @IBAction func cropButton(_ sender: UIButton) {
+        
+        
+        
+        let scale:CGFloat = 1/scrollView.zoomScale
+    
+        let x:CGFloat = scrollView.contentOffset.x * scale
+        let y:CGFloat = scrollView.contentOffset.y * scale
+        
+        let width:CGFloat = scrollView.frame.size.width * scale
+        let height:CGFloat = scrollView.frame.size.height * scale
+        let croppedCGImage = imageView.image?.cgImage?.cropping(to: CGRect(x: x, y: y, width: width, height: height))
+        
+        let croppedImage = UIImage(cgImage: croppedCGImage!)
+        
+        setImageToCrop(image: croppedImage)
+        
+
+        
+        imageView.image = croppedImage
+        
+    }
+    
+  
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        scrollView.delegate = self
+
+        setImageToCrop(image: UIImage(named: "dog")!)
     }
 
     override func didReceiveMemoryWarning() {
