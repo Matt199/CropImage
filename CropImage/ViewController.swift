acc1553d430f23dd041a218653af6f722377adbd
mateuszkopacz <m.kopacz199@gmail.com>
2017-10-17 21:01:10 +0200

    cleanup

--- CropImage/ViewController.swift
+++ CropImage/ViewController.swift
@@ -20,54 +20,65 @@ class ViewController: UIViewController, UIScrollViewDelegate {
     @IBOutlet weak var imageViewHeight: NSLayoutConstraint!
     
     
+
+    
+
+    
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         return imageView
     }
     
     func setImageToCrop(image: UIImage){
 
+        // Sets the image
         imageView.image = image
 
+        
+        // Sets constant image dimensions
         imageViewWidth.constant = image.size.width
         imageViewHeight.constant = image.size.height
 
+        
+     
         let scaleHeight = scrollView.frame.width / image.size.width
+
         let scaleWidth = scrollView.frame.height / image.size.height
 
         let maxScale = max(scaleWidth, scaleHeight)
 
+        scrollView.minimumZoomScale = maxScale
         scrollView.zoomScale = maxScale
-
        
-
+      
 
     }
  
     @IBAction func cropButton(_ sender: UIButton) {
         
-        
-        
-        let scale:CGFloat = 1/scrollView.zoomScale
+        let scale = 1 / scrollView.zoomScale
     
         let x:CGFloat = scrollView.contentOffset.x * scale
+        
         let y:CGFloat = scrollView.contentOffset.y * scale
         
         let width:CGFloat = scrollView.frame.size.width * scale
         let height:CGFloat = scrollView.frame.size.height * scale
+        
         let croppedCGImage = imageView.image?.cgImage?.cropping(to: CGRect(x: x, y: y, width: width, height: height))
         
         let croppedImage = UIImage(cgImage: croppedCGImage!)
         
-        setImageToCrop(image: croppedImage)
+        print("Height = \(imageView.frame.size.height)")
+        print("Width = \(imageView.frame.size.width)")
+        print("x = \(x)")
+        print("y = \(y)")
         
-
+        setImageToCrop(image: croppedImage)
+    
         
         imageView.image = croppedImage
         
     }
-    
-  
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -76,6 +87,8 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         scrollView.delegate = self
 
         setImageToCrop(image: UIImage(named: "dog")!)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
